9500e5c1320b59ed17276ef0030ec183bbd6b270
Ji Kim <jkim55@gmail.com>
2016-09-10 17:51:05 -0600

    posting works! Can add to-do items to list

--- api/index.js
+++ api/index.js
@@ -2,9 +2,16 @@
 
 const express = require('express')
 let router = express.Router()
-
+const pg = require('../db/knex_config.js')
 router.post('/v1/items', (req, res, next)=>{
-  res.json(req.body)
+  pg('todo').insert(req.body)
+  .then(()=>{
+    res.redirect('/')
+  })
+  .catch((err)=>{
+    console.error('Error caught in inserting into DB');
+    next(err)
+  })
 })
 
 
