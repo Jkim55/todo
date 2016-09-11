ca3082ecbcfd970427a74eca9ef8312d26e695d5
Ji Kim <jkim55@gmail.com>
2016-09-10 20:43:12 -0600

    user can input new to-do item and iput has been sanitized for <255 char

--- api/index.js
+++ api/index.js
@@ -3,6 +3,21 @@
 const express = require('express')
 let router = express.Router()
 const pg = require('../db/knex_config.js')
+
+router.post('/v1/items', (req, res, next)=>{
+  let name = req.body.name
+  let description = req.body.description
+
+  if(name.length > 255){
+    return res.redirect('/')
+  }
+
+  if(description.length > 255){
+    return res.redirect('/')
+  }
+  next()
+})
+
 router.post('/v1/items', (req, res, next)=>{
   pg('todo').insert(req.body)
   .then(()=>{
